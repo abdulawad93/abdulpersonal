d2584b8744ef918028475174c6827990c0d632b1
Abdul Rahman Awad <68397717+abdulawad93@users.noreply.github.com>
2020-08-05 14:35:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-document.querySelector(".msgTitle").addEventListener("keydown",function(event){
-  if(event.key!=="Tab")
-  document.querySelector("form").setAttribute("action","mailto:abdulrahman.y.awad@gmail.com?subject="+document.querySelector(".msgTitle").value+event.key);
+$(".msgTitle").on("keydown", function(event){
+  if(event.key!=="Tab"&&event.key!=="Backspace")
+  {
+    const receiver= $(".receiver-email").attr("value");
+    let subject= "?subject="+$(".msgTitle").val()+event.key;
+    console.log(subject);
+    $("form").attr("action","mailto:"+receiver+subject);
+  }
+});
+
+$(".nav-link").on('click', function(){
+    $(".navbar-nav").hide();
+});
+$(".navbar-toggler").on('click', function(){
+  $(".navbar-nav").show();
 });
